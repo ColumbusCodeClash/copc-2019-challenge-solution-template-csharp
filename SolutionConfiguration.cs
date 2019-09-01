9ae1809fd3555432d70e7eed0c12a1f59aabbd73
Russell Patterson <rwp.cpe@gmail.com>
2019-08-31 21:33:06 -0400

    Add Challenge property for the verification process.

--- SolutionConfiguration.cs
+++ SolutionConfiguration.cs
@@ -6,6 +6,7 @@ namespace COPC.Solution
     {
         public string TeamName => "Team Name";       // TODO: Return your team's "friendly" name here. This will show on the leaderboard.      
 
+        public Challenge Challenge => Challenge.AlphaMath;          // TODO: Return the Challenge that this solution is for.
         public ChallengeLevel ChallengeLevel => ChallengeLevel.NonBeginner;     // TODO: Return the level at which you're competing.
     }
 }
