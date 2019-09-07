1973257c765455cd332d2da4c20aa0712879224c
Russell Patterson <rwp.cpe@gmail.com>
2019-09-06 21:23:01 -0400

    Remove TODOs

--- SolutionConfiguration.cs
+++ SolutionConfiguration.cs
@@ -4,9 +4,8 @@ namespace COPC.Solution
 {
     public class SolutionConfiguration : ISolutionConfiguration
     {
-        public string TeamName => "Team Name";       // TODO: Return your team's "friendly" name here. This will show on the leaderboard.      
-
-        public Challenge Challenge => Challenge.AlphaMath;          // TODO: Return the Challenge that this solution is for.
-        public ChallengeLevel ChallengeLevel => ChallengeLevel.NonBeginner;     // TODO: Return the level at which you're competing.
+        public string TeamName => "Team Name";
+        public Challenge Challenge => Challenge.AlphaMath;
+        public ChallengeLevel ChallengeLevel => ChallengeLevel.NonBeginner;
     }
 }
